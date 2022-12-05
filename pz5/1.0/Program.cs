62794544a78efb1b31a84e173d3ad3a6191a5c2b
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-05 21:20:13 +0300

    1st zad

---
+++ pz5/1.0/Program.cs
@@ -0,0 +1,27 @@
+﻿void Mass(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0;i<n;i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+        Console.Write($"{array[i]} ");
+    }
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0;i<n;i++)
+    {
+        if (array[i] >= 0)
+        {
+            sum1 = sum1 + array[i];
+        } 
+        else
+        {
+            sum2 = sum2 +array[i];
+        }
+    }
+
+    Console.WriteLine($"Суума полож = {sum1}");
+    Console.WriteLine($"Суума отр = {sum2}");
+}
+int s = 12;
+Mass(s);
